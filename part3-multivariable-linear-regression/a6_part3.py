fa739b686a800bed42c5b8bce808e6542a07eac8
Maged <mmothana@cps.edu>
2023-12-13 10:28:00 -0600

    Part 3

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -7,16 +7,47 @@ from sklearn.model_selection import train_test_split
 data = pd.read_csv("part3-multivariable-linear-regression/car_data.csv")
 x = data[["miles","age"]].values
 y = data["Price"].values
+print(data)
 
 #split the data into training and testing data
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 
 #create linear regression model
+model = LinearRegression().fit(xtrain, ytrain)
 
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
-
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y), 2)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
 print("***************")
 print("Testing Results")
+print(f"Model's Linear Equation: y={coef[1]}x2+ {intercept}")
+print("R Squared value:", r_squared)
+'''
+X = x.reshape(-1, 1)
+predict = model.predict(10000)
+predict = np.around(predict, 2)
+print(predict)'''
+
+xtest = list(xtest)
+xtest.append([89000, 10])
+xtest.append([150000, 20])
+
+ytest = list(ytest)
+ytest.append(89000)
+ytest.append(150000)
+
+
+predict = model.predict(xtest)
+preidct = np.around(predict, 2)
+print(predict)
+
+for index in range(len(xtest)):
+    actual = ytest[index]
+    predicted_y = predict[index]
+    x_coord = xtest[index]
+    print(f"Miles: {x_coord[0]} Age: {x_coord[1]} Actual: {actual} Predicted: {predicted_y}")
--- part3-multivariable-linear-regression/a6_part3_writeup.md
+++ part3-multivariable-linear-regression/a6_part3_writeup.md
@@ -6,8 +6,16 @@ After completing `a6_part3.py` answer the following questions
 
 1. What is the R Squared coefficient for your model? What does that mean about the model in relation to your data?
 
+The r-squared coeffiecient is 0.86 which means that that the model is correct most of the time and is very accurate.
+
 2. Is your model accurate? Why or why not?
+
+This model is accurate as the r-squared value is close to one which means that it is quite accurate.
 
 3. What does the model predict a 10-year-old car with 89000 miles is worth? What about a car that is 20 years old with 150000 miles?
 
+a 10 year old car with 89000 miles is around $8999.85 and a 20 year old car with 150000 miles would be $2155.
+
 4. You may notice that some of your predicted results are negative. This is occurring when the value of age and the mileage of the car are very high. Why do you think this is happening?
+
+I assume it would be because the age and mileage is so high that those two values outweigh the original cost of the car which makes the car worthless.
